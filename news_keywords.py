dd882f7acb795a23906e22378292d085a2828473
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-02-11 22:49:15 +0300

    Keywords for website detection

---
+++ news_keywords.py
@@ -0,0 +1,3 @@
+news_keywords = {"erdoğan", "manşet", "son dakika", "haber", "ekonomi", "köşe yazısı", "borsa", "kılıçdaroğlu", "yazar",
+                 "gündem", "dünya", "hava durumu", "gazete", "lig", "futbol", "basketbol", "spor", "fenerbahçe",
+                 "galatasaray"}
